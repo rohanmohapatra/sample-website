4a86e604c57ec7fa5f0467a13a3ef8eb6a63c549
rohanmohapatra <rohanironcrest14@gmail.com>
2023-04-07 20:09:29 -0700

    feat: fix image loading

--- site-extractor/index.ts
+++ site-extractor/index.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 import fs from "fs";
 import Logger from "@ptkdev/logger";
+import puppeteer from "puppeteer";
+import https from "https";
+
+interface UrlName {
+  url: string;
+  name: string;
+}
 
 const pages = [
   "home",
@@ -11,6 +18,50 @@ const pages = [
   "join-us",
   "codedata",
 ];
+
+const buildImagesForSite = async (pageName: string) => {
+  const browser = await puppeteer.launch();
+  const [page] = await browser.pages();
+  await page.goto(`https://sites.google.com/view/micosyslab/${pageName}`);
+  let imageUrls = await page.evaluate(() =>
+    Array.from(document.querySelectorAll("img"), ({ src }) => src)
+  );
+
+  const backgroundImages = await page.evaluate(() => {
+    let backgroundImages = Array.from(
+      document.querySelectorAll("div"),
+      (el) => el.style.backgroundImage
+    );
+    backgroundImages = backgroundImages.filter((bg) => !!bg);
+    backgroundImages = backgroundImages.map((bg) => bg.split('"')[1]);
+    return backgroundImages;
+  });
+  imageUrls = imageUrls.concat(backgroundImages);
+
+  await browser.close();
+
+  // Create images directory
+  if (!fs.existsSync("../pages/images")) {
+    fs.mkdirSync("../pages/images");
+  }
+
+  const promises: Promise<UrlName>[] = [];
+  const images = new Map<string, string>();
+
+  imageUrls.forEach((imageUrl, i) => {
+    const promise = new Promise<UrlName>((resolve, reject) => {
+      https.get(imageUrl, (response) => {
+        const imageName = `../pages/images/${pageName}_${i}.jpg`;
+        response.pipe(fs.createWriteStream(imageName));
+        resolve({ url: imageUrl, name: `images/${pageName}_${i}.jpg` });
+      });
+    });
+    promises.push(promise);
+  });
+
+  const values = await Promise.all(promises);
+  return values;
+};
 
 const logger = new Logger();
 
@@ -38,15 +89,22 @@ pages.forEach((page) => {
       // TODO: Pretty it later
       logger.warning("HTML is not pretty yet");
 
-      fs.writeFileSync(`../pages/${page}.html`, htmlPage);
-      logger.info("Wrote to html file");
+      // Use Local Images
+      buildImagesForSite(page).then((images: UrlName[]) => {
+        images.forEach(({ url, name }) => {
+          htmlPage = htmlPage.replaceAll(url, name);
+        });
 
-      // Create index.html for home page
-      if (page == "home") {
-        fs.writeFileSync(`../pages/index.html`, htmlPage);
-      }
+        fs.writeFileSync(`../pages/${page}.html`, htmlPage);
+        logger.info("Wrote to html file");
 
-      logger.sponsor(`Successful Build for ${page}`);
+        // Create index.html for home page
+        if (page == "home") {
+          fs.writeFileSync(`../pages/index.html`, htmlPage);
+        }
+
+        logger.sponsor(`Successful Build for ${page}`);
+      });
     })
     .catch((error) => {
       console.error(error);
